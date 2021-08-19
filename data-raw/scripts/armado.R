c3fba32201f468181612824a828f2a782b8ac729
Guidowe <guidowe45@gmail.com>
2021-08-19 12:58:54 -0300

    referencia para armado paquete

--- data-raw/scripts/armado.R
+++ data-raw/scripts/armado.R
@@ -7,12 +7,12 @@ devtools::load_all()
 suma(1:10)
 #check() # Es el chequeo general del paquete
 use_mit_license("Guido Weksler")
-document()
-install()
+  devtools::document()
+  devtools::install()
 use_testthat()
 
 library(testthat)
-test()
+devtools::test()
 use_package("dplyr")
 use_package("tidyr")
 use_package("purrr")
@@ -31,6 +31,7 @@ use_test("isco_08_to_isco_88_4digit")
 usethis::use_github()
 browse_github_token()
 use_readme_rmd()
+devtools::build()
 ##On Windows the collection of tools needed for building packages from source is called Rtools. Rtools is NOT an R package. It is NOT installed with install.packages(). Instead, download it from https://cran.r-project.org/bin/windows/Rtools/ and run the installer.
 #usethis::use_build_ignore("notes")#.Rbuildignore is a way to resolve some of the tension between the practices that support your development process and CRAN’s requirements for submission and distribution
 
